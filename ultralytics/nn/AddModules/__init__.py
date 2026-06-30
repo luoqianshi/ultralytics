8c3c7621f963f7a78e36fc54cc13141c1315137c
luoqianshi <1309251728@qq.com>
2026-06-30 16:55:57 +0800

    尝试改进 2-2：部分加载预训练权重，使用A2C2f_Mona替换原始的A2C2f，在SSDC-UAV数据集上进行了训练和测试。

--- ultralytics/nn/AddModules/__init__.py
+++ ultralytics/nn/AddModules/__init__.py
@@ -2,10 +2,10 @@
 # from .SimAM import *
 # from .AssemFormer import *
 # from .HSFPN import *
-# from .Mona import *
+from .Mona import *
 # # from .HPDown import *
 # from .ESMoE import *
-from .DySample import *
+# from .DySample import *
 
 
 
